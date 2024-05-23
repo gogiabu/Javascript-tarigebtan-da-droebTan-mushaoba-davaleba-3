e34c252e0190f527f3b237a5d5c800c23f68d026
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-05-23 19:31:07 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var dro = new Date();
+document.write(dro.getFullYear() + ("<br>") );
+document.write(dro.getDate() + ("<br>") );
+document.write(dro.getMinutes() + ("<br>") );
+document.write(dro.getSeconds() + ("<br>") );
+document.write(dro.getDay() + ("<br>") );
